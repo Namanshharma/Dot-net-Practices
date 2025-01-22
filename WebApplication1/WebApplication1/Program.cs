30c9688eadcca4f301b62a427d4f88fd3a6fee15
Naman Sharma <namansharma812@gmail.com>
2025-01-23 04:09:31 +0530

    Here is the basic go through of the functionalities of Program.cs file

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,6 +1,33 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello !");
+
+app.Use(async (HttpContext context, RequestDelegate requestDelegate) =>
+{
+    await context.Response.WriteAsync("This is just another middleware");
+});
+
+app.Run(async (HttpContext context) =>
+{
+    context.Response.StatusCode = 200;
+    context.Request.Headers.ContentType = "text/plain";
+    context.Response.Headers.ContentType = "text/plain";
+    context.Response.Headers["My Key"] = "My Value";
+
+    if (context.Request.Method == "Get")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+        }
+    }
+    await context.Response.WriteAsync("Hello, I have set the status code to 204");
+});
 
 app.Run();
+
+// if we want to set the Reverse proxy server then we can setup that in LaunchSetting.json file. There are 2 profiles in that, first one is for the Kestrel server
+// which is the by default server provided and another server we can configure according to our requirement
+
+// we have some of the middlewares present :- Map , Run 
--- WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15034a5bfcf09f8903054a7a11b74065d955487b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
