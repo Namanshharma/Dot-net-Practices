7a2b1ccea1ff0e034c3487a463748e7d8dc03017
Naman Sharma <namansharma812@gmail.com>
2025-01-25 04:47:04 +0530

    DI concept

--- WebApplication2/ClassLibrary/CitiesService.cs
+++ WebApplication2/ClassLibrary/CitiesService.cs
@@ -8,13 +8,13 @@ public class CitiesService
         cities = new List<string> { "Amritsar", "Jalandhar", "Mohali", "Chandigarh", "Panchkula", "Hauz Khaz" };
     }
     // From the controller prespective :- Create a method which will fetch the data of cities
-    public List<string> getCities()
+    public List<string> GetCities()
     {
         return cities;
     }
-    public string setCity(string cityName)
+    public string SetCity(string cityName)
     {
         cities.Add(cityName);
-        return $"{cityName} is successfully added";
+        return $"{cityName} is added successfully";
     }
 }
--- WebApplication2/WebApplication2/Controllers/CityController.cs
+++ WebApplication2/WebApplication2/Controllers/CityController.cs
@@ -11,12 +11,17 @@ namespace WebApplication2.Controllers
         private readonly CitiesService _citiesService;
         public CityController()
         {
-            _citiesService = new CitiesService();
+            _citiesService = new CitiesService(); // here this is the bad practice that we are creating an instance of service but instead of this we need to register this service and then use it by using DI
+                                                  // right now it is called DIRECT DEPENDENCY 
         }
         [HttpGet]
         public IEnumerable GetCities()
         {
-            return _citiesService.getCities();
+            return _citiesService.GetCities();
         }
     }
 }
+
+// _citiesService = new CitiesService(); <<---- this is a problematic thing becoz by doing this controller is directly dependent on service. So the solution of this problem is DIP
+// The upper approach have DEPENDENCY PROBLEM which means the higher level class is dependent on the lower level class. which creates a problem
+// DIP ( DEPENDENCY INVERSION PRINCIPLE ) <<---- It says that the Higher level class must not be dependent on the lower level class instead both must depend upon ABSTRACTION.
