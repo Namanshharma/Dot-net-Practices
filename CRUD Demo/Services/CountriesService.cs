37a75432ac9ec0a3f82768d4587335d92dbd1591
Naman Sharma <namansharma812@gmail.com>
2025-02-09 00:45:29 +0530

    Update databse and now code is refactored

--- CRUD Demo/Services/CountriesService.cs
+++ CRUD Demo/Services/CountriesService.cs
@@ -4,46 +4,29 @@ using ServiceContracts.DTO;
 namespace Services;
 public class CountriesService : ICountriesService
 {
-    // private readonly List<Country> _countries;
-    private readonly CRUDDbContext _databaseDbContext;
-    public CountriesService(CRUDDbContext databaseDbContext, bool isInitialize = true)
-    {
-        _databaseDbContext = databaseDbContext;
-        // _countries = new List<Country>();
-        // if (isInitialize)
-        // {
-        //     _countries.AddRange(new List<Country>{
-        //     new Country { CountryId = Guid.Parse("15e6d2c4-87ba-4120-a4f2-128739120dca"), CountryName = "India" },
-        //     new Country { CountryId = Guid.Parse("7d5fdc7a-1c24-4d85-a322-9dd32b32c730"), CountryName = "Pankistan" },
-        //     new Country { CountryId = Guid.Parse("d6e2bff4-2a60-4b65-a3a8-906713109c12"), CountryName = "China" },
-        //     new Country { CountryId = Guid.Parse("a4a9fbf9-747f-4cb0-9606-4b2622d1a30a"), CountryName = "Nepal" },
-        //     new Country { CountryId = Guid.Parse("a8ecceb1-bbfd-45a3-82b7-bd79667421c9"), CountryName = "Bhutan" }
-        //     });
-        // }
-    }
+    private readonly CRUDDbContext _db;
+    public CountriesService(CRUDDbContext db) { _db = db; }
     public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
     {
         if (countryAddRequest == null) { throw new ArgumentNullException(nameof(countryAddRequest)); }
         if (countryAddRequest.CountryName == null) { throw new ArgumentNullException(nameof(countryAddRequest.CountryName)); }
-        // if (_countries.Where(x => x.CountryName == countryAddRequest.CountryName).Count() > 0) { throw new ArgumentException("Given country already exists"); }
-        if (_databaseDbContext.Countries.Where(x => x.CountryName == countryAddRequest.CountryName).Count() > 0) { throw new ArgumentException("Given country already exists"); }
+        if (_db.Countries.Count(x => x.CountryName == countryAddRequest.CountryName) > 0) { throw new ArgumentException("Given Country name is already present"); }
+
         Country country = countryAddRequest.ToCountry();
         country.CountryId = Guid.NewGuid();
-        // _countries.Add(country);
-        _databaseDbContext.Countries.Add(country);
-        _databaseDbContext.SaveChanges();
+        _db.Countries.Add(country);
+        _db.SaveChanges();                      
+
         return country.ToCountryResponse();
     }
     public List<CountryResponse> GetAllCountries()
     {
-        return _databaseDbContext.Countries.Select(x => x.ToCountryResponse()).ToList();
-        // return _countries.Select(x => x.ToCountryResponse()).ToList();
+        return _db.Countries.Select(x => x.ToCountryResponse()).ToList();
     }
     public CountryResponse? GetCountryByCountryId(Guid? countryId)
     {
         if (countryId == null) { throw new ArgumentNullException(countryId.ToString() + "argument is null"); }
-        // Country? country = _countries.FirstOrDefault<Country>(x => x.CountryId == countryId);
-        Country? country = _databaseDbContext.Countries.FirstOrDefault<Country>(x => x.CountryId == countryId);
+        Country? country = _db.Countries.FirstOrDefault<Country>(x => x.CountryId == countryId);
         if (country == null) { return null; }
         return country?.ToCountryResponse();
     }
