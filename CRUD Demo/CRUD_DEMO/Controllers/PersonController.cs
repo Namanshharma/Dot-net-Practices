04d4ac92f6e59abf7097f1da8cf9cc635a0a60ae
Naman Sharma <namansharma812@gmail.com>
2025-02-11 03:29:45 +0530

    Code implmentaiton of Repository

--- CRUD Demo/CRUD_DEMO/Controllers/PersonController.cs
+++ CRUD Demo/CRUD_DEMO/Controllers/PersonController.cs
@@ -9,20 +9,20 @@ namespace CRUD_DEMO.Controllers
         private readonly IPersonsService _personsService;
         public PersonController(IPersonsService personsService, ICountriesService countriesService)
         {
-            _countriesService = countriesService; _personsService = personsService;
+            // _countriesService = countriesService; _personsService = personsService;
         }
         // [Route("person/index")]
         // [Route("/")]
-        public ActionResult Index()
-        {
-            List<PersonResponse> persons = _personsService.GetAllPersons();
-            return View(persons);
-        }
+        // public ActionResut Index()
+        // {
+        //     List<PersonResponse> persons = _personsService.GetAllPersons();
+        //     return View(persons);
+        // }
         // [Route("person/create")]
         // [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+        // public IActionResult Create()
+        // {
+        //     return View();
+        // }
     }
 }
--- CRUD Demo/CRUD_DEMO/Program.cs
+++ CRUD Demo/CRUD_DEMO/Program.cs
@@ -1,5 +1,7 @@
 using Entities;
 using Microsoft.EntityFrameworkCore;
+using Repositories;
+using RepositoryContracts;
 using ServiceContracts;
 using Services;
 
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+
 builder.Services.AddScoped<IPersonsService, PersonsService>();
 builder.Services.AddScoped<ICountriesService, CountriesService>();
 builder.Services.AddDbContext<CRUDDbContext>(x =>
