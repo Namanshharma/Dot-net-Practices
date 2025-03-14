788ce60d2938a4ad6a12e9437cc9d4df6c2a955b
Naman Sharma <namansharma812@gmail.com>
2025-03-14 19:09:04 +0530

    Swagger setup

--- CitiesManager/CitiesManager.WebAPI/DbContext/ApplicationDBContext.cs
+++ CitiesManager/CitiesManager.WebAPI/DbContext/ApplicationDBContext.cs
@@ -10,8 +10,7 @@ public class ApplicationDBContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<City>().HasData(new City() = { CityId = Guid.Parse("b19feae6-e804-4765-8234-6505dfa1a74c"), CityName = "Amritsar" });
-
-        modelBuilder.Entity<City>().HasData(new City() = { CityId = Guid.Parse("b4c9a780-15dd-4395-9246-e10fba41a9f4"), CityName = "Chandigarh" });
+        // modelBuilder.Entity<City>().HasData(new City() = { CityId = Guid.Parse("b19feae6-e804-4765-8234-6505dfa1a74c"), CityName = "Amritsar" });
+        // modelBuilder.Entity<City>().HasData(new City() = { CityId = Guid.Parse("b4c9a780-15dd-4395-9246-e10fba41a9f4"), CityName = "Chandigarh" });
     }
 }
--- CitiesManager/CitiesManager.WebAPI/Program.cs
+++ CitiesManager/CitiesManager.WebAPI/Program.cs
@@ -7,10 +7,16 @@ builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDBContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
+builder.Services.AddEndpointsApiExplorer(); // generated description for all endpoints
+builder.Services.AddSwaggerGen();   // Generates open API specification
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
     app.MapOpenApi();
+
+app.UseSwagger();   // creates endpoints for Swagger.json
+app.UseSwaggerUI(); // creates Swagger UI for testing all web API endpoint
 
 app.UseHsts();          // force browsers to use Http 
 app.UseHttpsRedirection();
